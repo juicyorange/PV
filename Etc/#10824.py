ec232b8b272947eb7072f13315309327c9343c2b
juicyorange <rlaxodud9809@khu.ac.kr>
2022-03-18 17:48:02 +0900

    #Etc 2022/03/18

---
+++ Etc/#10824.py
@@ -0,0 +1,25 @@
+'''
+2022/03/18
+boj.kr/10824
+
+네 수
+
+문제:
+네 자연수 A, B, C, D가 주어진다. 이때, A와 B를 붙인 수와 C와 D를 붙인 수의 합을 구하는 프로그램을 작성하시오.
+두 수 A와 B를 합치는 것은 A의 뒤에 B를 붙이는 것을 의미한다. 즉, 20과 30을 붙이면 2030이 된다.
+
+입력 :
+첫째 줄에 네 자연수 A, B, C, D가 주어진다. (1 ≤ A, B, C, D ≤ 1,000,000)
+
+결과 :
+A와 B를 붙인 수와 C와 D를 붙인 수의 합을 출력한다.
+'''
+import sys
+input = sys.stdin.readline
+
+a, b, c, d = map(int, input().split())
+
+
+AB = str(a) + str(b)
+CD = str(c) + str(d)
+print(int(AB) + int(CD))
