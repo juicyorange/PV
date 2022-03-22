a95e828cebfd6d5b40a123609fbeed2870863f98
juicyorange <rlaxodud9809@khu.ac.kr>
2022-03-22 17:53:17 +0900

    #Etc 2022/03/21

---
+++ Etc/#10430.py
@@ -0,0 +1,28 @@
+'''
+2022/03/21
+boj.kr/10430
+
+나머지
+
+문제 :
+(A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+
+(A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+
+세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+
+입력 :
+첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+
+결과 :
+첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+'''
+import sys
+input = sys.stdin.readline
+
+A, B, C = map(int, input().split())
+
+print((A+B) % C)
+print(((A % C) + (B % C)) % C)
+print((A*B) % C)
+print(((A % C) * (B % C)) % C)
